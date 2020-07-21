cee49f8b2e1c723d90a501b2a58bd886d8a26d6a
zxq <461890276@qq.com>
2020-07-21 23:48:17 +0800

    add-permission-page-finished

--- mock/index.js
+++ mock/index.js
@@ -3,10 +3,12 @@ const { param2Obj } = require('./utils')
 
 const user = require('./user')
 const table = require('./table')
+const role = require('./role')
 
 const mocks = [
   ...user,
-  ...table
+  ...table,
+  ...role
 ]
 
 // for front mock
